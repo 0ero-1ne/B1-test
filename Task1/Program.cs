8585e02b72b38142a9a0109d2d49ceff40f0ccb5
0ero1ne <volikovd17@gmail.com>
2024-11-01 16:54:52 +0300

    feat(Task 1): Files concatenating

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Threading.Tasks;
-using Task1.Generators;
+﻿using Task1.Generators;
 
 namespace Task1
 {
@@ -35,15 +33,16 @@ namespace Task1
 
         static void Generate(string[] args) // generate console command
         {
+            // Так, как приложение консольное, стоит проверить входные параметры
             if (args.Length < 3)
             {
-                PrintError("Syntax error.\nGenerate syntax is: task1 generate -o folder_to_save");
+                PrintError("Syntax error.\nGenerate syntax is: task1 generate -o dir_to_save");
                 return;
             }
 
             if (args[1] != "-o" || args[2] == "")
             {
-                PrintError("Syntax error.\nGenerate syntax is: task1 generate -o folder_to_save");
+                PrintError("Syntax error.\nGenerate syntax is: task1 generate -o dir_to_save");
                 return;
             }
 
@@ -51,7 +50,7 @@ namespace Task1
 
             if (!Directory.Exists(directoryPath))
             {
-                PrintError("No such folder. Check the path");
+                PrintError("No such directory. Check the path");
                 return;
             }
 
@@ -61,7 +60,7 @@ namespace Task1
 
             Console.WriteLine($"Generating files into {fullPath}");
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var watch = System.Diagnostics.Stopwatch.StartNew(); // Для подсчёта времени выполнения алгоритма
 
             for (int i = 1; i <= 100; i++)
             {
@@ -69,6 +68,7 @@ namespace Task1
                 var queue = new Queue<string>();
                 object locker = new();
 
+                // Распараллелим вычисления для ускорения создания контента для файла
                 Parallel.For(0, 5000, (i) => {
                     var strings = "";
 
@@ -101,7 +101,80 @@ namespace Task1
 
         static void Concat(string[] args) // concat console command
         {
-            Console.WriteLine("Concat");
+            if (args.Length < 5)
+            {
+                PrintError("Syntax error.\nGenerate syntax is: task1 concat -f files_dir -p pattern");
+                Console.WriteLine("Leave pattern like \"\" for empty string");
+                return;
+            }
+
+            if (args[1] != "-f" || args[2] == "" || args[3] != "-p")
+            {
+                PrintError("Syntax error.\nGenerate syntax is: task1 concat -f files_dir -p pattern");
+                Console.WriteLine("Leave pattern like \"\" for empty string");
+                return;
+            }
+
+            var directoryPath = args[2];
+
+            if (!Directory.Exists(directoryPath))
+            {
+                PrintError("No such directory. Check the path");
+                return;
+            }
+            directoryPath = directoryPath.Last() == '\\' ? directoryPath : directoryPath + '\\'; // Убедимся, что не стоит лишних символов "\"
+
+            // Contains("") вернёт true всегда
+            // Так, как символ октоторпа в файле не используется, то заменим пустую строку на него
+            // Также надо учитывать, что паттерн чувствителен к регистру
+            var pattern = args[4] == "" ? "#" : args[4]; 
+
+            var content = new Queue<string>();
+            long deletedRows = 0;
+            object locker = new();
+
+            Console.WriteLine($"Concatenating files into {directoryPath}concatenated.txt");
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            Parallel.For(0, 5, (i) =>
+            {
+                var queue = new Queue<string>();
+                for (int f = i * 20 + 1; f <= i * 20 + 20; f++)
+                {
+                    using StreamReader reader = new(directoryPath + $"file{f}.txt", System.Text.Encoding.UTF8, true, 4096);
+                    var str = reader.ReadLine();
+                    while (str != null && str != "\n")
+                    {
+                        if (str.Contains(pattern))
+                        {
+                            deletedRows++;
+                        }
+                        else
+                        {
+                            queue.Enqueue(str + '\n');
+                        }
+                        str = reader.ReadLine();
+                    }
+                    reader.Close();
+                }
+                lock (locker)
+                {
+                    foreach (var item in queue)
+                    {
+                        content.Enqueue(item);
+                    }
+                }
+            });
+
+            using StreamWriter writer = new(directoryPath + $"concatenated.txt", false);
+            foreach (var item in content)
+            {
+                writer.Write(item);
+            }
+            watch.Stop();
+
+            Console.WriteLine($"Concatenating done in {watch}");
+            Console.WriteLine($"{deletedRows} rows where deleted");
         }
 
         static void PrintError(string error)
